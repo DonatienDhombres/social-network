ba628ca817fd8f67ba77e7c2993c070fa5c686e2
akatsukifood <jean.delaleau@gmail.com>
2021-08-15 14:50:32 +0200

    Lesson 15 & 16, Create the Profile Model and Get current User Profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,11 +1,38 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
+const { check, validationResult } = require('express-validator/check')
+
+const Profile = require('../../models/Profile')
+const User = require('../../models/User')
+
+// @route    GET api/profile/me
+// @desc     Get current users profile
+// @access   Private 
 
 
-// @route    GET api/profile
-// @desc     Test route
-// @access   Public 
+router.get('/me', auth, async (req, res) => {
+    //Le middleware auth permet de sécuriser la requete
+    //On a accès à req.user, grâce au middleware auth
+    try {
+        const profile = await Profile.findOne({ user: req.user.id }).populate('user', ['name', 'avatar']);
 
-router.get('/', (req, res) => res.send('Profile route'));
+        if (!profile) {
+            return res.status(400).json({ msg: 'There is no profile for this user' });
+        }
+
+        res.json(profile)
+    } catch (err) {
+        console.log(err.message);
+        res.status(500).send('Server Error');
+    }
+});
+
+// @route    POST api/profile
+// @desc     Create or update user profile
+// @access   Private 
+
+
+
 
 module.exports = router;
