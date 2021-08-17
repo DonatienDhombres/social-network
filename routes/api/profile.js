6a8fa029ca52ab3e30637b1f88f6f58582921948
akatsukifood <jean.delaleau@gmail.com>
2021-08-17 11:02:17 +0200

    Lesson 17 - Get all profiles & profile by user ID

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -35,8 +35,10 @@ router.get('/me', auth, async (req, res) => {
 router.post('/',
     [
         auth,
-        check('status', 'Status is required').not().isEmpty(),
-        check('skills', 'Skills is required').not().isEmpty()
+        [
+            check('status', 'Status is required').not().isEmpty(),
+            check('skills', 'Skills is required').not().isEmpty()
+        ]
     ],
     async (req, res) => {
         const errors = validationResult(req);
@@ -86,28 +88,64 @@ router.post('/',
         try {
             let profile = await Profile.findOne({ user: req.user.id });
 
+            //Update
             if (profile) {
-                //Update
                 profile = await Profile.findOneAndUpdate(
                     { user: req.user.id },
                     { $set: profileFields },
-                    { new: true },
-                    { useFindAndModify: false }
+                    { new: true }
                 );
 
                 return res.json(profile);
             }
 
-            //Create
+
+            // Create
             profile = new Profile(profileFields);
 
             await profile.save();
             res.json(profile);
         } catch (err) {
-            console.log(err.message);
+            console.error(err.message);
             res.status(500).send('Server Error')
         }
     })
+
+
+// @route    GET api/profile
+// @desc     Get all profiles
+// @access   Public
+
+router.get('/', async (req, res) => {
+    try {
+        const profiles = await Profile.find().populate('user', ['name', 'avatar']);
+        res.json(profiles);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+})
+
+
+// @route    GET api/profile/user/:user_id
+// @desc     Get profile by user ID
+// @access   Public
+
+router.get('/user/:user_id', async (req, res) => {
+    try {
+        const profile = await Profile.findOne({ user: req.params.user_id }).populate('user', ['name', 'avatar']);
+
+        if (!profile) return res.status(400).json({ msg: 'Profile not found' });
+
+        res.json(profile);
+    } catch (err) {
+        console.error(err.message);
+        if (err.kind == 'ObjectId') {
+            return res.status(400).json({ msg: 'Profile not found' });
+        }
+        res.status(500).send('Server Error');
+    }
+})
 
 
 module.exports = router;
