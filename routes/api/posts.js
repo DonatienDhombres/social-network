169613f071b47bbc0488fc17b6489d1f9adef66c
Donatien Dhombres <donatien.dbr@gmail.com>
2021-08-18 18:11:47 +0200

    25. Add post route

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -8,10 +8,66 @@ const User = require('../../models/User');
 const Post = require('../../models/Post');
 
 
-// @route    GET api/posts
-// @desc     Test route
-// @access   Public 
+// @route    POST api/posts
+// @desc     Create post
+// @access   Private 
 
-router.get('/', (req, res) => res.send('Posts route'));
+router.post('/', [
+    auth,
+    [
+        check('text', 'Text is required')
+            .not()
+            .isEmpty()
+    ]
+],
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+        try {
+            const user = await User.findById(req.user.id).select('-password');
+
+            const newPost = {
+                text: req.body.text,
+                name: user.name,
+                avatar: user.avatar,
+                user: req.user.id
+            }
+
+
+            //Create
+            const post = new Post(newPost);
+            await post.save();
+            res.json(post);
+
+            //Update
+            // let existingPost = await Post.findOne({});
+            // if (existingPost) {
+            //     //Update
+            //     return res.json(existingPost)
+            // }
+
+
+
+
+
+        } catch (err) {
+            console.error(err.message);
+            res.status(500).send('Server error')
+        }
+    });
+
+router.get('/:post_id', async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.post_id);
+        res.send(post);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server error')
+    }
+});
+
 
 module.exports = router;
